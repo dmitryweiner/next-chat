7f91b77e63b66d1ae3ba3c9cdc06b6fd014933e5
dmitryweiner <dmitry.weiner@gmail.com>
2020-06-05 16:50:41 +0700

    Creating basic routes

---
+++ pages/form.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import React from "react";
+
+function Form() {
+  return <>
+    <Link href="/">Back</Link>
+  </>;
+}
+
+export default Form;
---
+++ pages/messages.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import React from "react";
+
+function Messages() {
+  return <>
+      <Link href="/">Back</Link>
+    </>;
+}
+
+export default Messages;
